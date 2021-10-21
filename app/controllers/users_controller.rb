f88517f9bbcd245cea9cee9f9721c2534875735f
GANESH-DESHMUK <dganesh@vardhaman.org>
2021-10-21 19:55:23 +0530

    updated user controller for creting user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
         user = User.find(id)
         render plain: user.to_pleasent_string
  end
+
+ def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+
+    resultant_text = "Hello, #{name} we have created a user for you with email: #{email}"
+    render plain: resultant_text
+  end
 end
