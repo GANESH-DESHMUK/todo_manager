fd2096bd5c99f1fdb981abb2bfaa7067cff4dc43
GANESH-DESHMUK <dganesh@vardhaman.org>
2021-10-21 19:38:03 +0530

    made changes

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,30 +1,7 @@
 class User < ActiveRecord::Base
 
-    def index
-        response_text = "List of Users"
-        render plain: User.all.map { |user| user.to_pleasent_string }.join("\n")
-      end
-    
-      def show
-        id = params[:id]
-        user = User.find(id)
-        render plain: user.to_pleasant_string
-      end
-    
-      def create
-        name = params[:name]
-        email = params[:email]
-        password = params[:password]
-    
-        new_user = User.create!(
-          name: name,
-          email: email,
-          password: password,
-        )
-    
-        response_text = "Hey, #{name} we have created a user for you with email: #{email}"
-        render plain: response_text
-      end
-    
+  def to_pleasent_string
+    "#{id}. #{name}, #{email}"
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,29 @@ class UsersController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def index
-      render plain: User.all.map { |users| usrs.to_pleasent_string }.join("\n")
-    end
+        resultant_text = "Below are list of users:"
+        render plain: User.all.map { |user| user.to_pleasent_string }.join("\n")
+      end
+    
+      def show
+        id = params[:id]
+        user = User.find(id)
+        render plain: user.to_pleasant_string
+      end
+    
+      def create
+        name = params[:name]
+        email = params[:email]
+        password = params[:password]
+    
+        new_user = User.create!(
+          name: name,
+          email: email,
+          password: password,
+        )
+    
+        resultant_text = "Hey, #{name} we have created a user for you with email: #{email}"
+        render plain: response_text
+      end
+    
 end
