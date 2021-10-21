cdb9efd0f805d0308f5b1433d0384f7a318e1c46
GANESH-DESHMUK <dganesh@vardhaman.org>
2021-10-21 19:27:41 +0530

    cleared errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-    
+    skip_before_action :verify_authenticity_token
+
     def index
-      render plain: User.all.map{ |user| user.to_pleasent_string }.join("\n")
+      render plain: User.all.map { |user| user.to_pleasent_string }.join("\n")
     end
 end
