923fb7d41a5de754aa0a313c2ba71baf8658b729
GANESH-DESHMUK <dganesh@vardhaman.org>
2021-10-21 20:15:03 +0530

    update users_controller to skip authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+
+    skip_before_action :verify_authenticity_token
  def index
     render plain: User.all.map { |user|  user.to_pleasent_string }.join("\n")
  end 
