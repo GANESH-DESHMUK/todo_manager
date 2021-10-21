e8e26685b0b12f39cdbf0fecf3f3eef1ef00929d
GANESH-DESHMUK <dganesh@vardhaman.org>
2021-10-21 20:42:23 +0530

    formateed using rufo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
     skip_before_action :verify_authenticity_token
  def index
     render plain: User.all.map { |user|  user.to_pleasent_string }.join("\n")
