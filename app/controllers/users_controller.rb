9e0ea55cef2d44480e33c2fae94d33cc100364c8
GANESH-DESHMUK <dganesh@vardhaman.org>
2021-10-21 19:51:00 +0530

    render particular id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
  def index
     render plain: User.all.map { |user|  user.to_pleasent_string }.join("\n")
- end     
+ end 
+ 
+ def show
+    id = params[:id]
+        user = User.find(id)
+        render plain: user.to_pleasent_string
+ end
 end
