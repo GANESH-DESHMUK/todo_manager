764a4070a51abd77f3e36678da0e5f44154b85c4
GANESH-DESHMUK <dganesh@vardhaman.org>
2021-10-22 20:12:10 +0530

    removed commented lines in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  #get "todos", to: "todos#index"
-  #post "todos/create", to: "todos#create"
   resources :todos
   resources :users
   post "users/login", to: "users#login"
