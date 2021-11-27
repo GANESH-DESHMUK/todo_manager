4e2f65d970e5a17e343a6c6386f9559184602196
GANESH-DESHMUK <dganesh@vardhaman.org>
2021-11-27 15:43:08 +0530

    implement signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get "/" => "home#index"
+  post "users/login", to: "users#login"
   resources :todos
   resources :users
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
-  post "users/login", to: "users#login"
+  delete "/signout" => "sessions#destroy", as: :destroy_session
 end
